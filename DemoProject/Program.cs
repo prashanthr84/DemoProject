978f5ff5b27d2298bae21ceb1e8def3ff43d16f0
prashanthr <prashanthr84@gmail.com>
2022-06-04 20:00:34 +0530

    Feature/add calculator (#1)
    * added calculator
    
    * renames variables

--- DemoProject/Program.cs
+++ DemoProject/Program.cs
@@ -6,7 +6,16 @@ namespace DemoProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var calculator = new Calculator();
+            var result = calculator.Add(2, 3);
+        }
+    }
+
+    internal class Calculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
         }
     }
 }
